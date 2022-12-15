925151d463351c7aaf671584106e6074cd02b437
JXue Soo <u2102841@siswa.um.edu.my>
2022-12-15 15:12:02 +0800

    Update Profile Student and Teacher UI

---
+++ app/src/main/java/com/example/finderx_mad/StudentProfile.java
@@ -0,0 +1,14 @@
+package com.example.finderx_mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_profile);
+    }
+}
--- app/src/main/java/com/example/finderx_mad/announcementActivity.java
+++ app/src/main/java/com/example/finderx_mad/announcementActivity.java
@@ -23,6 +23,11 @@ public class announcementActivity extends AppCompatActivity {
 
         s1 = getResources().getStringArray(R.array.lecturerName);
         s2 = getResources().getStringArray(R.array.TaskContent);
+      //  s1[0] = " Dr Hafiezah";
+        //s1[1] = "Dr Ang";
+        //s2[0]=  " This is the Task Content";
+       // s2[1]= "hello world";
+
 
         myAdapter myAdapter = new myAdapter(this,s1,s2);
         recyclerView.setAdapter(myAdapter);
