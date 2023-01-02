a8247ed850a37271237ad7dd61f408282debca99
JXue Soo <u2102841@siswa.um.edu.my>
2023-01-02 22:16:51 +0800

    Read Data From Firebase, Student Profile

--- app/src/main/java/com/example/finderx_mad/StudentProfileFragment.java
+++ app/src/main/java/com/example/finderx_mad/StudentProfileFragment.java
@@ -3,14 +3,26 @@ package com.example.finderx_mad;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +30,16 @@ import android.widget.RatingBar;
  * create an instance of this fragment.
  */
 public class StudentProfileFragment extends Fragment {
+    //private FirebaseAuth student;
+    private FirebaseUser student;
+    private FirebaseFirestore FirebaseStore;
+    private DocumentReference df;
+
+   String StudentID;
+    //private String StudentName;
+    //private String StudentGmail;
+   // private String StudentMajor;
+    //private String StudentTelephone;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -67,16 +89,50 @@ public class StudentProfileFragment extends Fragment {
         RatingBar RBRating = view.findViewById(R.id.RBRating);
         //RBRating.setRating(AvgRating);
         RBRating.setIsIndicator(true);
-        Button BtnEdit = view.findViewById(R.id.BtnEdit);
-        BtnEdit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getActivity(),EditProfile.class));
-            }
-        });
+
+        EditText editText = view.findViewById(R.id.ETStudentDescription2);
+        ImageView imageView = view.findViewById(R.id.IVEditPen);
+        imageView.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             editText.setEnabled(true);
+                                         }
+                                     });
+                TextView TVStudentName =(TextView)  view.findViewById(R.id.TVStudentName);
+                TextView TVStudentID = (TextView) view.findViewById(R.id.TVStudentID);
+                TextView TVStudentMajor = (TextView) view.findViewById(R.id.TVStudentMajor);
+                TextView TVStudentPhone = (TextView) view.findViewById(R.id.TVStudentPhone);
+                TextView TVStudentEmail = (TextView) view.findViewById(R.id.TVStudentEmail);
+
+                // Connect to Firebase
+                student = FirebaseAuth.getInstance().getCurrentUser();
+                StudentID = student.getUid();
+                FirebaseStore = FirebaseFirestore.getInstance();
+                df = FirebaseStore.collection("User").document(StudentID);
+                df.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            TVStudentName.setText(document.getString("Name"));
+                            TVStudentID.setText(document.getString("StudentID"));
+                            TVStudentEmail.setText(document.getString("Email"));
+                            TVStudentMajor.setText(document.getString("Majoring"));
+                            TVStudentPhone.setText(document.getString("Phone"));
+                            if (document.exists()) {
+                                Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                            } else {
+                                Log.d("TAG", "No such document");
+                            }
+                        } else {
+                            Log.d("TAG", "get failed with ", task.getException());
+                        }
+                    }
+                });
 
         return view;
     }
+
     //to calculate the average rating and display them
         /*float sum=0;
         int numRatings = ratings.size();
