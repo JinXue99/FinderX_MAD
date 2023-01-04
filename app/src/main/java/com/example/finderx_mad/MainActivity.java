063090b24f9264dc397a65b1a60e25b39bdde14d
LEEKEATEN <u2102836@siswa.um.edu.my>
2023-01-04 20:15:52 +0800

    Teacher Nav

--- app/src/main/java/com/example/finderx_mad/MainActivity.java
+++ app/src/main/java/com/example/finderx_mad/MainActivity.java
@@ -10,12 +10,18 @@ import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
+import com.example.finderx_mad.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
@@ -24,54 +30,59 @@ import com.example.finderx_mad.R.id;
 import com.example.finderx_mad.R.layout;
 import com.google.android.material.button.MaterialButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
-    private ActionBarDrawerToggle toggle;
-    private DrawerLayout drawerLayout;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.TBMainAct);
-        setSupportActionBar(toolbar);
-        drawerLayout = findViewById(R.id.DLMain);
-        toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar,
-                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        NavHostFragment host = (NavHostFragment)
-                getSupportFragmentManager().findFragmentById(R.id.NHFMain);
-        NavController navController = host.getNavController();
-        AppBarConfiguration appBarConfiguration = new
-                AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this,
-                navController, appBarConfiguration);
+        //Binding things
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        setupNavMenu(navController);
+
+        ArrayList<String> users = new ArrayList<>();
+        users.add("Student");
+        users.add("Lecturer");
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                R.layout.custom_spinner,
+                users
+
+        );
+        adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown);
+        binding.sUser.setAdapter(adapter);
+
+        //When Login button is clicked
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = binding.sUser.getSelectedItem().toString();
+
+                if (user.equals("Student")){
+                    Intent is = new Intent(MainActivity.this, StudentActivity.class);
+                    startActivity(is);
+                }else if(user.equals("Lecturer")){
+                    Intent it = new Intent(MainActivity.this, TeacherActivity.class);
+                    startActivity(it);
+                }
+
+
+            }
+        });
     }
 
-    private void setupNavMenu(NavController navController){
-
-        NavigationView sideNav = findViewById(R.id.sideNav);
-
-        NavigationUI.setupWithNavController(sideNav, navController, false);
-
-
+    @Override //To avoid memory leaks
+    protected void onDestroy(){
+        super.onDestroy();
+        binding = null;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        try {
-            Navigation.findNavController(this,
-                    R.id.NHFMain).navigate(item.getItemId());
-            return true;
-        }
-        catch (Exception ex)
-        {
-            return super.onOptionsItemSelected(item);
-        }
-    }
+
+
+
 
 
 }
