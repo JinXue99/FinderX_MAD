42ebe0b152311c6aafde77e75ab7b3d2d64d2af7
LEEKEATEN <u2102836@siswa.um.edu.my>
2023-01-06 16:16:28 +0800

    Teacher Upload Task Function

--- app/src/main/java/com/example/finderx_mad/TeacherViewTask2Fragment.java
+++ app/src/main/java/com/example/finderx_mad/TeacherViewTask2Fragment.java
@@ -3,21 +3,20 @@ package com.example.finderx_mad;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TeacherViewTaskFragment#newInstance} factory method to
+ * Use the {@link TeacherViewTask2Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TeacherViewTaskFragment extends Fragment {
+public class TeacherViewTask2Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +27,7 @@ public class TeacherViewTaskFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TeacherViewTaskFragment() {
+    public TeacherViewTask2Fragment() {
         // Required empty public constructor
     }
 
@@ -38,11 +37,11 @@ public class TeacherViewTaskFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TeacherAddTaskFragment.
+     * @return A new instance of fragment TeacherViewTask2Fragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TeacherViewTaskFragment newInstance(String param1, String param2) {
-        TeacherViewTaskFragment fragment = new TeacherViewTaskFragment();
+    public static TeacherViewTask2Fragment newInstance(String param1, String param2) {
+        TeacherViewTask2Fragment fragment = new TeacherViewTask2Fragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -63,13 +62,14 @@ public class TeacherViewTaskFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_teacher_view_task,null,false);
+        View view = inflater.inflate(R.layout.fragment_teacher_view_task2,null,false);
 
         ImageView ivAddTaskButton = (ImageView) view.findViewById(R.id.ivAddTaskButton);
         ivAddTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getContext().getApplicationContext(),"Add Task Button is Clicked",Toast.LENGTH_SHORT).show();
+                Navigation.findNavController(view).navigate(R.id.DestTeacherAddNewTask);
 
             }
         });
