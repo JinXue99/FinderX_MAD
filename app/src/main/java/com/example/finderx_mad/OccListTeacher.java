9eb7951666573eab56c973947f00e3ff7d608587
LEEKEATEN <u2102836@siswa.um.edu.my>
2023-01-11 02:27:47 +0800

    Complete Teacher 1 Course, 1 Occ, view its task and grp

---
+++ app/src/main/java/com/example/finderx_mad/OccListTeacher.java
@@ -0,0 +1,20 @@
+package com.example.finderx_mad;
+//OccList2001Teacher
+public class OccListTeacher {
+    String Occ;
+
+    public OccListTeacher(String occ) {
+        Occ = occ;
+    }
+
+    public OccListTeacher() {
+    }
+
+    public String getOcc() {
+        return Occ;
+    }
+
+    public void setOcc(String occ) {
+        Occ = occ;
+    }
+}
--- app/src/main/java/com/example/finderx_mad/TeacherOccFragment.java
+++ app/src/main/java/com/example/finderx_mad/TeacherOccFragment.java
@@ -2,41 +2,45 @@ package com.example.finderx_mad;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TeacherOccFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+//TeacherOcc2001Fragment
 public class TeacherOccFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    RecyclerView recview;
+    OccListTeacherAdapter adapter;
+    ArrayList<OccListTeacher> list;
+    FirebaseDatabase database;
+    DatabaseReference myRef, CourseRef;
+    OccListTeacher viewOccTeacher;
 
     public TeacherOccFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TeacherOccFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static TeacherOccFragment newInstance(String param1, String param2) {
         TeacherOccFragment fragment = new TeacherOccFragment();
         Bundle args = new Bundle();
@@ -59,6 +63,38 @@ public class TeacherOccFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_teacher_occ, container, false);
+        View view = inflater.inflate(R.layout.fragment_teacher_occ, null, false);
+
+        //Firebase
+        recview = (RecyclerView) view.findViewById(R.id.RVOccListTeacher);
+        database = FirebaseDatabase.getInstance("https://finderx-6cd15-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        myRef = database.getReference("Teacher").child("Course Code").child("C1").child("Occ");
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext().getApplicationContext());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        recview.setLayoutManager(layoutManager);
+
+        list = new ArrayList<>();
+        adapter = new OccListTeacherAdapter(getContext().getApplicationContext(), list);
+        recview.setAdapter(adapter);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    viewOccTeacher = dataSnapshot.getValue(OccListTeacher.class);
+                    list.add(viewOccTeacher);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return view;
     }
 }
