0e602ea8273e43b2eb2f43444a5818eef4a64ee0
JXue Soo <u2102841@siswa.um.edu.my>
2023-01-12 06:55:00 +0800

    add restriction during create group, and show the group chat list according the members inside

--- app/src/main/java/com/example/finderx_mad/CreateNewGroup.java
+++ app/src/main/java/com/example/finderx_mad/CreateNewGroup.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,8 +16,19 @@ import androidx.fragment.app.Fragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,10 +38,12 @@ import com.google.firebase.database.FirebaseDatabase;
 public class CreateNewGroup extends Fragment {
 
 
-    EditText ETGroupName,etTM1, etTM2, etTM3, etTM4, etTM5;
+    EditText ETGroupName, ETTM1, ETTM2, ETTM3, ETTM4, ETTM5;
+    FirebaseAuth mAuth;
     FirebaseDatabase database;
-    DatabaseReference myRef;
+    DatabaseReference myRef, userRef,chatRef;
     SGroupListMAD group;
+    String currentUserID;
 
 
     private static final String ARG_PARAM1 = "param1";
@@ -78,13 +92,86 @@ public class CreateNewGroup extends Fragment {
 
         database = FirebaseDatabase.getInstance("https://finderx-6cd15-default-rtdb.asia-southeast1.firebasedatabase.app/");
         myRef = database.getReference("Student Group List MAD").child("Teams");
+        mAuth = FirebaseAuth.getInstance();
+        //emailRef = database.getReference("Users").child(mAuth.getUid()).child("Email");
 
         ETGroupName = (EditText) view.findViewById(R.id.ETGroupName);
-        etTM1 = (EditText) view.findViewById(R.id.etTM1);
-        etTM2 = (EditText) view.findViewById(R.id.etTM2);
-        etTM3 = (EditText) view.findViewById(R.id.etTM3);
-        etTM4 = (EditText) view.findViewById(R.id.etTM4);
-        etTM5 = (EditText) view.findViewById(R.id.etTM5);
+        ETTM1 = (EditText) view.findViewById(R.id.etTM1);
+        ETTM2 = (EditText) view.findViewById(R.id.etTM2);
+        ETTM3 = (EditText) view.findViewById(R.id.etTM3);
+        ETTM4 = (EditText) view.findViewById(R.id.etTM4);
+        ETTM5 = (EditText) view.findViewById(R.id.etTM5);
+
+        // Set ETTM1 as the gmail of the current user
+        currentUserID = mAuth.getCurrentUser().getUid();
+        userRef = database.getReference("Users").child(currentUserID).child("Email");
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    ETTM1.setText(snapshot.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        //Check the validation of the email
+
+//        DatabaseReference tentativeRef = database.getReference("Email");
+//        tentativeRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                Button BtnPublish = (Button) view.findViewById(R.id.BtnPublish);
+//                BtnPublish.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        if (ETGroupName.getText().toString().isEmpty()) {
+//                            ETGroupName.setError("Group Name is required!");
+//                            return;
+//                                    }
+//                                if (!ETTM2.getText().toString().equals(snapshot.child("1").getValue().toString())) {
+//                                    ETTM2.setError("This email does not exist");
+//                                    return;
+//                                } else if (ETTM2.getText().toString().isEmpty()) {
+//                                    CreateNewGroup(group);
+//                                }
+//                                if (!ETTM3.getText().toString().equals(snapshot.child("2").getValue().toString())) {
+//                                        ETTM3.setError("This email does not exist");
+//                                        return;
+//                                    } else if (ETTM3.getText().toString().isEmpty()) {
+//                                        CreateNewGroup(group);
+//                                    }
+//                                    if (!ETTM4.getText().toString().equals(snapshot.child("3").getValue().toString())) {
+//                                        ETTM4.setError("This email does not exist");
+//                                        return;
+//                                    } else if (ETTM4.getText().toString().isEmpty()) {
+//                                        CreateNewGroup(group);
+//                                    }
+//                                    if (!ETTM5.getText().toString().equals(snapshot.child("4").getValue().toString())) {
+//                                        ETTM5.setError("This email does not exist");
+//                                        return;
+//                                    }
+//                                    //at least one member in group
+//                                    else if (ETTM5.getText().toString().isEmpty()) {
+//                                        CreateNewGroup(group);
+//                                    }
+//
+//
+//                                }
+//                            });
+//                        }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//
+//        });
 
         Button BtnPublish = (Button) view.findViewById(R.id.BtnPublish);
         BtnPublish.setOnClickListener(new View.OnClickListener() {
@@ -94,11 +181,41 @@ public class CreateNewGroup extends Fragment {
                     ETGroupName.setError("Group Name is required!");
                     return;
                 }
-                if (etTM1.getText().toString().isEmpty()) {
-                    etTM1.setError("At least one member is required!");
-                    return;
-                }
-                CreateNewGroup(group);
+                DatabaseReference tentativeRef = database.getReference("Email");
+                tentativeRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        if(ETTM2.getText().toString().isEmpty()){
+                            ETTM2.setError("This Field cannot be empty");
+                        } else if (!ETTM2.getText().toString().equals(snapshot.child("1").getValue().toString())) {
+                            ETTM2.setError("This email does not exist");
+                            return;
+                        }if(ETTM3.getText().toString().isEmpty()) {
+                            CreateNewGroup(group);
+                        }else if (!ETTM3.getText().toString().equals(snapshot.child("2").getValue().toString())) {
+                            ETTM3.setError("This email does not exist");
+                            return;
+                        } if (ETTM4.getText().toString().isEmpty()) {
+                            CreateNewGroup(group);
+                        } else if (!ETTM4.getText().toString().equals(snapshot.child("3").getValue().toString())) {
+                            ETTM4.setError("This email does not exist");
+                            return;
+                        } if (ETTM5.getText().toString().isEmpty()) {
+                            CreateNewGroup(group);
+                        } else if (!ETTM5.getText().toString().equals(snapshot.child("4").getValue().toString())) {
+                            ETTM5.setError("This email does not exist");
+                            return;
+                        }
+
+                        }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+
+                });
             }
         });
 
@@ -109,20 +226,31 @@ public class CreateNewGroup extends Fragment {
     public void CreateNewGroup(SGroupListMAD group) {
         SGroupListDB newgroup = new SGroupListDB(
                 ETGroupName.getText().toString(),
-                etTM1.getText().toString(), etTM2.getText().toString(),
-                etTM3.getText().toString(), etTM4.getText().toString(),
-                etTM5.getText().toString());
+                ETTM1.getText().toString(), ETTM2.getText().toString(),
+                ETTM3.getText().toString(), ETTM4.getText().toString(),
+                ETTM5.getText().toString());
 
         myRef.child(newgroup.getTName()).setValue(newgroup).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(getContext().getApplicationContext(), "Group is Created!", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     Toast.makeText(getContext().getApplicationContext(), "Unsuccessful to create group", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+        chatRef = database.getReference();
+        chatRef.child("Group").child(ETGroupName.getText().toString()).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            //Toast.makeText(getContext().getApplicationContext(),groupName+ " is created", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
 
     }
 
--- app/src/main/java/com/example/finderx_mad/StudentGroupListFragment.java
+++ app/src/main/java/com/example/finderx_mad/StudentGroupListFragment.java
@@ -117,10 +117,10 @@ public class StudentGroupListFragment extends Fragment {
         FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Boolean status = snapshot.child("Status").getValue(Boolean.class);
-                if (status){
-                    create.setVisibility(View.GONE);
-                }
+ //               Boolean status = snapshot.child("Status").getValue(Boolean.class);
+//                if (status){
+//                    create.setVisibility(View.GONE);
+//                }
             }
 
             @Override
