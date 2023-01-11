0d919c589e96f31628a42d08d9dbc1a360a28818
LEEKEATEN <u2102836@siswa.um.edu.my>
2023-01-12 07:39:08 +0800

    Complete teacher navigation and onboarding pages

--- app/src/main/java/com/example/finderx_mad/CreateNewGroup.java
+++ app/src/main/java/com/example/finderx_mad/CreateNewGroup.java
@@ -39,7 +39,7 @@ public class CreateNewGroup extends Fragment {
 
 
 
-    EditText ETGroupName,etTM1, etTM2, etTM3, etTM4, etTM5;
+    EditText ETGroupName,ETTM1,ETTM2,ETTM3,ETTM4,ETTM5;
 
 
     FirebaseDatabase database;
@@ -94,7 +94,7 @@ public class CreateNewGroup extends Fragment {
 
         database = FirebaseDatabase.getInstance("https://finderx-6cd15-default-rtdb.asia-southeast1.firebasedatabase.app/");
         myRef = database.getReference("Student Group List MAD").child("Teams");
-        mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         //emailRef = database.getReference("Users").child(mAuth.getUid()).child("Email");
 
         ETGroupName = (EditText) view.findViewById(R.id.ETGroupName);
